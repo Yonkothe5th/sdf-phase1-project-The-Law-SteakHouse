f355084254bad06fa7e8d32329f13558645d637d
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-11-03 21:12:42 +0300

    Java Script Update

--- docs/index.js
+++ docs/index.js
@@ -1,20 +1,87 @@
 document.addEventListener("DOMContentLoaded", function (){
-    const baseURL = "https://my-json-server.typicode.com/Yonkothe5th/sdf-phase1-project-The-Law-SteakHouse"
+    const baseURL = "http://localhost:3000"
     
-    const menuItemOne = document.getElementById("Itemone")
-    const menuItemTwo = document.getElementById("Itemtwo")
-    const menuItemThree = document.getElementById("Itemthree")
+    const menuItemOne = document.getElementById("itemone")
+    const menuItemTwo = document.getElementById("itemtwo")
+    const menuItemThree = document.getElementById("itemthree")
+    const suggestionsList = document.getElementById("suggestion-list")
 
-    function fetchMenu (){
-        return fetchJSON(`$(baseURL}/meals`)
-        .then(resp => resp.json())
-        .then(funtion (){
-        return populateMenu (meals)
+
+    fetch(`${baseURL}/meals`)
+    .then (resp => resp.json())
+    .then(data => {
+
+        menuItemOne.innerHTML = `'<h3 
+        id="firstitem"><strong>${data[0].name}</strong>
+        </h3>
+        <img id="substituteimgone"
+        alt="Picture of First meal goes here"
+        src = "${data[0].image}"
+        />
+        <p 
+        id = 'desciption of first meal'>${data[0].description}</p>
+        </p>
+        <p 
+        id = 'price of the first meal'>${data[0].price}
+        </p>`   
+
+        menuItemTwo.innerHTML = `<h3
+        id="seconditem"><strong>${data[1].name}</strong>
+        </h3>
+        <img id="substituteimgtwo"
+        alt="Picture of Second meal goes here"
+        src = "${data[1].image}"
+        />
+        <p 
+        id = 'desciption of Second meal'>${data[1].description}
+        </p>
+        <p 
+        id = 'price of the Second meal'>${data[1].price}
+        </p>`
+
+        menuItemThree.innerHTML = `<h3 
+        id="thirditem"><strong>${data[2].name}</strong>
+        </h3>
+        <img id="substituteimgthree"
+        alt="Picture of firstmeal goes here"
+        src = "${data[2].image}"
+        />
+        <p 
+        id = 'desciption of Third meal'>${data[2].description}
+        </p>
+        <p 
+        id = 'price of the third meal'>${data[2].price}
+        </p>`
+
+
+        function generateReviewList(suggestions) {
+            let suggestionsList = '';
+            for (let i = 0; i < suggestions.length; i++) {
+            suggestionsList += '<li data-index="' + i + '">' + suggestions[i] + '</li>';
+            }
+            return suggestionsList;
         }
-    }
-    function populateMenu(data){
-        menuItemOne.document.getElementByid('firstitem') = data.name;
+        suggestionsList.innerHTML = '<ul id="suggestion-list">'
+        '<li>' + generateReviewList(data[3].suggestion)+'</li>'
+        '</ul>'
+
+    })
+
+
+
+})
+
+
+
+
+
+
+
+    // fetchMenuData(`${baseURL}/meals/1`)
+    // .then(populateMenu)
+    // .catch(logError);
+
     
-        }
-    } 
-)
+// fetch('http://localhost:3000/meals')
+// .then(res => res.json())
+// .then (data => {console.log(data)})
