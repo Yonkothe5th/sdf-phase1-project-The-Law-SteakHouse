867d50af16688b9634577772f912ed50f40e46a5
Brian Lawi Ochiel <lawioochiel@gmail.com>
2023-11-05 19:46:00 +0300

    submit event listener

--- docs/index.js
+++ docs/index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function (){
     const menuItemTwo = document.getElementById("itemtwo")
     const menuItemThree = document.getElementById("itemthree")
     const suggestionsList = document.getElementById("suggestion-list")
-
+    const suggestionForm = document.getElementById('suggestion-form');
 
 
     fetch(`${baseURL}/meals`)
@@ -53,70 +53,73 @@ document.addEventListener("DOMContentLoaded", function (){
         <p 
         id = 'price of the third meal'>${data[2].price}
         </p>`
-    })
-
-    // fetch(`${baseURL}/suggestions`)
-    // .then(resp => resp.json())
-    // .then(data => { 
-    //         console.log(data)
-    //         const li = document.querySelector('#first-suggestion')
-    //         li.textContent = (data[0].suggestion)
-    //     }
-    // )
+    });
 
     // Function to fetch suggestions and populate them in an unordered list
-    function fetchAndPopulateSuggestions() {
-        fetch(`${baseURL}/suggestions`) // Replace with the actual URL of your server
+function fetchAndPopulateSuggestions() {
+    fetch(`${baseURL}/suggestions`) 
         .then(response => response.json())
         .then(data => {
-            // const suggestionList = document.getElementById('suggestion-list'); // Assuming you have an <ul> with this ID
-            suggestionsList.innerHTML = ''; // Clear the existing list
+            const suggestionList = document.getElementById('suggestion-list');
+            suggestionList.innerHTML = '';
 
-            data.forEach(suggestions =>{
-            const li = document.createElement('li');
-            li.textContent = suggestions.suggestion;
-            suggestionsList.appendChild(li);
+            data.forEach(suggestions => {
+                const li = document.createElement('li');
+                li.textContent = suggestions.suggestion;
+                suggestionList.appendChild(li);
             });
         })
         .catch(error => {
-        console.error('Error fetching suggestions:', error);
-    });
+            console.error('Error fetching suggestions:', error);
+        });
+}
+
+// Function to add a new suggestion
+function addSuggestion(newSuggestion) {
+    fetch(`${baseURL}/suggestions`)
+        .then(response => response.json())
+        .then(data => {
+            data.push({
+                id: data.length + 1,
+                suggestion: newSuggestion,
+            });
+
+            return fetch(`${baseURL}/suggestions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+        })
+        .then(response => {
+            if (response.ok) {
+                fetchAndPopulateSuggestions();
+            } else {
+                console.error('Error updating the database:', response.status, response.statusText);
+            }
+        })
+        .catch(error => {
+            console.error('Error adding a suggestion:', error);
+        });
+}
+
+// Event listener for the form submission
+const suggestionsForm = document.getElementById('suggestions-form');
+suggestionsForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const suggestionInput = document.getElementById('suggestion');
+    const newSuggestion = suggestionInput.value;
+
+    if (newSuggestion) {
+        addSuggestion(newSuggestion);
+        suggestionInput.value = '';
     }
+});
 
-  // Call the function to fetch and populate suggestions
-    fetchAndPopulateSuggestions();
-
-        // suggestionsList.innerHTML = '<ul id="suggestion-list">'+
-        // '<li>' + populateSuggestionList(data) +'</li>'+
-        // '</ul>'
-    
-
-    
-    //     function populateSuggestionList (data) {
-    //         let suggestionsList = '';
-    //         for (let i = 0; i < suggestions.length; i++) {
-    //         suggestionsList += '<li data-index="' + i + '">' + suggestions[i] + '</li>';
-    //         }
-    //         return suggestionsList;
-    //     }
-    // )
-
-    //     suggestionsList.innerHTML = '<ul id="suggestion-list">'+
-    //     '<li>' + populateSuggestionList(data) +'</li>'+
-    //     '</ul>'
-})
+// Call the function to fetch and populate suggestions on page load
+fetchAndPopulateSuggestions();
 
 
-
-
-
-
-
-    // fetchMenuData(`${baseURL}/meals/1`)
-    // .then(populateMenu)
-    // .catch(logError);
-
-    
-// fetch('http://localhost:3000/meals')
-// .then(res => res.json())
-// .then (data => {console.log(data)})
+});
